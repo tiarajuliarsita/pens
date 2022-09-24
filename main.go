91c8bb383c2d35f229fd6af68cb5e741b2d1aa21
tiarajuliarsita <tiarajuliarsita@gmail.com>
2022-09-24 21:59:52 +0800

    refactoring

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 )
 
@@ -16,16 +18,66 @@ type response struct {
 	Data    interface{} `json:"data"`
 }
 
+var pens []pen
+
+func sendresponse(code int, message string, data interface{}, w http.ResponseWriter) {
+	resp := response{
+		Code:    code,
+		Data:    data,
+		Message: message,
+	}
+	dataByte, err := json.Marshal(resp)
+	if err != nil {
+		resp := response{
+			Code:    http.StatusInternalServerError,
+			Data:    nil,
+			Message: "internal server error",
+		}
+		dataByte, _ = json.Marshal(resp)
+
+	}
+	w.Header().Set("content-type", "application/json")
+	w.WriteHeader(code)
+	w.Write(dataByte)
+
+}
+
 func main() {
 	http.HandleFunc("/api/v1/pens", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("succes"))
+		if r.Method == http.MethodGet {
+			sendresponse(http.StatusOK,"succes",pens, w)
+			return
+		}
+		if r.Method == http.MethodPost {
+			dataByte, err:= io.ReadAll(r.Body)
+			if err != nil{
+				sendresponse(http.StatusBadRequest, "bad request", nil, w)
+			}
+			defer r.Body.Close()
+			var pen pen
+			err = json.Unmarshal(dataByte, &pen)
+			if err != nil{
+				sendresponse(http.StatusInternalServerError, "internal server error", nil, w)
+			}
+
+			pens = append(pens, pen)
+			sendresponse(http.StatusCreated, "success", nil, w)
+			return
+		}
+
+		if r.Method == http.MethodPut {
+			w.Write([]byte("ini put"))
+			return
+		}
+		if r.Method == http.MethodDelete {
+			w.Write([]byte("ini delete"))
+			return
+		}
+		w.Write([]byte("wrong method"))
 	})
 
-	port:="8000"
+	port := "8000"
 	fmt.Println("server run on port", port)
-	http.ListenAndServe(":"+port,nil)
-		
-	}
-		
-	
+	http.ListenAndServe(":"+port, nil)
 
+}
