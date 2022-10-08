19e09ad2ee12d2df0ce8b8c05bbdea8099cef139
tiarajuliarsita <tiarajuliarsita@gmail.com>
2022-10-08 16:11:12 +0800

    refact models and repository

--- main.go
+++ main.go
@@ -11,19 +11,9 @@ import (
 
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
+	"github.com/tiarajuliarsita/pens/models"
+	"github.com/tiarajuliarsita/pens/repository"
 )
-
-type Pen struct {
-	ID    int    `json:"id"`
-	Name  string `json:"name"`
-	Price int    `json:"price"`
-}
-
-type response struct {
-	Code    int         `json:"code"`
-	Message string      `json:"message"`
-	Data    interface{} `json:"data"`
-}
 
 //var pens []pen
 func getpen(w http.ResponseWriter, r *http.Request) {
@@ -31,14 +21,14 @@ func getpen(w http.ResponseWriter, r *http.Request) {
 }
 
 func sendresponse(code int, message string, data interface{}, w http.ResponseWriter) {
-	resp := response{
+	resp := models.Response{
 		Code:    code,
 		Data:    data,
 		Message: message,
 	}
 	dataByte, err := json.Marshal(resp)
 	if err != nil {
-		resp := response{
+		resp := models.Response{
 			Code:    http.StatusInternalServerError,
 			Data:    nil,
 			Message: "internal server error",
@@ -50,10 +40,6 @@ func sendresponse(code int, message string, data interface{}, w http.ResponseWri
 	w.WriteHeader(code)
 	w.Write(dataByte)
 
-}
-
-func remove(slice []Pen, s int) []Pen {
-	return append(slice[:1], slice[s+1:]...)
 }
 
 var db *sql.DB
@@ -78,25 +64,10 @@ func main() {
 	defer db.Close()
 	r := mux.NewRouter()
 	r.HandleFunc("/api/v1/pens", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println(123, db == nil)
-		rows, err := db.Query("select id, name, price from pens")
+		pens, err := repository.GetPens(db)
 		if err != nil {
-			sendresponse(http.StatusBadRequest, "internal server eror", nil, w)
-		}
-		var pens []Pen
-		fmt.Println(rows == nil)
-		for rows.Next() {
-			var pen Pen
-
-			err = rows.Scan(
-				&pen.ID,
-				&pen.Name,
-				&pen.Price,
-			)
-			if err != nil {
-				sendresponse(http.StatusInternalServerError, "internal server", nil, w)
-			}
-			pens = append(pens, pen)
+			sendresponse(http.StatusBadRequest, "Internal server error, getpens", err.Error(), w)
+			return
 		}
 
 		sendresponse(http.StatusOK, "succes", pens, w)
@@ -112,26 +83,17 @@ func main() {
 			return
 		}
 
-		rows, err := db.Query("select id, name,price from pens where id = $1", id)
+		// rows, err := db.Query("select id, name,price from pens where id = $1", id)
 		if err != nil {
 			sendresponse(http.StatusInternalServerError, "internal server error, fail convert string to int", nil, w)
 			return
 		}
 
-		var pen Pen
-		if rows.Next() {
-			err = rows.Scan(
-				&pen.ID,
-				&pen.Name,
-				&pen.Price,
-			)
-
-			if err != nil {
-				sendresponse(http.StatusInternalServerError, "internal server error, fail convert string to int", nil, w)
-				return
-			}
+		pen, err :=repository.GetPen(db, id)
+		if err != nil {
+			sendresponse(http.StatusInternalServerError, "internal server error, fail convert string to int", err.Error(), w)
+			return
 		}
-
 		if pen.ID == 0 {
 			if err != nil {
 				sendresponse(http.StatusNotFound, "data not found", nil, w)
@@ -145,7 +107,7 @@ func main() {
 			return
 		}
 		defer r.Body.Close()
-		var newpen Pen
+		var newpen models.Pen
 		err = json.Unmarshal(dataByte, &newpen)
 		if err != nil {
 			sendresponse(http.StatusInternalServerError, "internal server error", nil, w)
@@ -155,9 +117,9 @@ func main() {
 		pen.Name = newpen.Name
 		pen.Price = newpen.Price
 
-		_, err = db.Exec("UPDATE pens SET name=$2, Price=$3 WHERE id=$1", pen.ID, pen.Name, pen.Price)
+		err=repository.UpdatePens(db, pen)
 		if err != nil {
-			sendresponse(http.StatusInternalServerError, "internal server error, update pens", err.Error(), w)
+			sendresponse(http.StatusInternalServerError, "internal server error", err.Error(), w)
 			return
 		}
 
@@ -174,16 +136,15 @@ func main() {
 			return
 		}
 		defer r.Body.Close()
-		var pen Pen
+		var pen models.Pen
 		err = json.Unmarshal(dataByte, &pen)
 		if err != nil {
-			sendresponse(http.StatusInternalServerError, "internal server error", nil, w)
+
 			return
 		}
-		_, err = db.Exec("insert into pens(name,price) values($1,$2)", pen.Name, pen.Price)
+		err = repository.CreatePens(db, pen)
 		if err != nil {
-			sendresponse(http.StatusInternalServerError, "internal server error, get pens", nil, w)
-			return
+			sendresponse(http.StatusInternalServerError, "internal server error", err, w)
 		}
 
 		sendresponse(http.StatusCreated, "success", nil, w)
@@ -192,32 +153,17 @@ func main() {
 	}).Methods(http.MethodPost)
 
 	r.HandleFunc("/api/v1/pens/{id}", func(w http.ResponseWriter, r *http.Request) {
-		
+
 		//id := r.URL.Query().Get("id")
 		id := mux.Vars(r)["id"]
 		if id == "" {
 			sendresponse(http.StatusBadRequest, "bad request, data id params is null", nil, w)
 			return
 		}
-
-		rows, err := db.Query("select id, name,price from pens where id = $1", id)
+		pen, err := repository.GetPen(db, id)
 		if err != nil {
-			sendresponse(http.StatusInternalServerError, "internal server error, fail convert string to int", nil, w)
+			sendresponse(http.StatusInternalServerError, "internal server error,get pen return err", err.Error(), w)
 			return
-		}
-
-		var pen Pen
-		if rows.Next() {
-			err = rows.Scan(
-				&pen.ID,
-				&pen.Name,
-				&pen.Price,
-			)
-
-			if err != nil {
-				sendresponse(http.StatusInternalServerError, "internal server error, fail convert string to int", nil, w)
-				return
-			}
 		}
 
 		if pen.ID == 0 {
@@ -227,13 +173,12 @@ func main() {
 			}
 		}
 
-		_, err = db.Exec("DELETE FROM pens  WHERE id=$1", pen.ID)
+		err = repository.Delete(db, id)
 		if err != nil {
-			sendresponse(http.StatusInternalServerError, "internal server error,delete pens return err", nil, w)
+			sendresponse(http.StatusInternalServerError, "internal server error,get pen return err", err.Error(), w)
 			return
 		}
-		sendresponse(http.StatusCreated, "success delete", nil, w)
-		return
+		sendresponse(http.StatusOK, "success delete", nil, w)
 	}).Methods(http.MethodDelete)
 
 	port := "8000"
